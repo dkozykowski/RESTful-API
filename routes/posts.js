45204e8f8837298c135d10d98be9b2a670a5ee81
dkozykowski <dkozykowski@gmail.com>
2020-04-21 19:58:56 +0200

    created model and first record

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => {
     res.send('We are on ebebe');
@@ -7,6 +8,21 @@ router.get('/', (req, res) => {
 
 router.get('/2', (req, res) => {
     res.send('We are on ebebe');
+});
+
+router.post('/', (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+
+    post.save()
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.json({ message: err});
+    });
 });
 
 module.exports = router;
