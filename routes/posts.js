e38a7c71b1df9a8b597ef137cadc3bebc7534544
dkozykowski <dkozykowski@gmail.com>
2020-04-28 14:16:32 +0200

    created DELETE and PATCH, updated GET

--- routes/posts.js
+++ routes/posts.js
@@ -2,27 +2,67 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('We are on ebebe');
+
+// returns all of the posts from the database
+router.get('/', async (req, res) => {
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err){
+        res.json({message:err});
+    }
+});
+
+// return a specific post
+router.get('/:postID', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.postID);
+        res.json(post);
+    }catch(err){
+        res.json({message: err});
+    }
 });
 
 router.get('/2', (req, res) => {
     res.send('We are on ebebe');
 });
 
-router.post('/', (req, res) => {
+// posts a post to database
+router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
-
-    post.save()
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => {
-        res.json({ message: err});
-    });
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);  
+    }catch(err){
+        res.json({message: err});
+    }
 });
+
+// delete a post from database
+router.delete('/:postID', async (req, res) => {
+    try{
+        const removedPost = await Post.remove({_id: req.params.postID });
+        res.json(removedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+// update a post
+router.patch('/:postID', async (req, res) => {
+    try{
+        const updatedPost = await Post.updateOne(
+            {_id: req.params.postID }, 
+            { $set : { title: req.body.title} } 
+        ); 
+        res.json(updatedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
 
 module.exports = router;
